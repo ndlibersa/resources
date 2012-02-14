485a4997a0bcff09c282dcf76a7cba596b591a55
Jaron Kennel <jaronkk@gmail.com>
2012-02-14 16:14:05 -0500

    Changed numberLoaded to be varchar to add flexibility

--- install/cataloging_upgrade.sql
+++ install/cataloging_upgrade.sql
@@ -1,6 +1,6 @@
 ALTER TABLE  `Resource` ADD  `recordSetIdentifier` VARCHAR( 45 ) NULL DEFAULT NULL ,
 ADD  `hasOclcHoldings` varchar( 10 ) NULL DEFAULT NULL ,
-ADD  `numberLoaded` INT( 11 ) NULL DEFAULT NULL ,
+ADD  `numberLoaded` VARCHAR( 45 ) NULL DEFAULT NULL ,
 ADD  `bibSourceURL` VARCHAR( 2000 ) NULL DEFAULT NULL ,
 ADD  `catalogingType` VARCHAR( 45 ) NULL DEFAULT NULL ,
 ADD  `catalogingStatus` VARCHAR( 45 ) NULL DEFAULT NULL

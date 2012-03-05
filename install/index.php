d584eb719b11b9ba4b911a7f295839658021c63e
Hekman Library Technology Team <libsysadmin@calvin.edu>
2012-03-05 16:46:46 -0500

    Change password fields to type="password"

--- install/index.php
+++ install/index.php
@@ -490,7 +490,7 @@ if ($step == "3"){
 			<tr>
 				<td>&nbsp;Database Password</td>
 				<td>
-					<input type="text" name="database_password" size="30" value="<?php echo $database_password?>">
+					<input type="password" name="database_password" size="30" value="<?php echo $database_password?>">
 				</td>
 			</tr>
 			<tr>
@@ -538,7 +538,7 @@ if ($step == "3"){
 			<tr>
 				<td>&nbsp;Database Password</td>
 				<td>
-					<input type="text" name="database_password" size="30" value="<?php echo $database_password?>">
+					<input type="password" name="database_password" size="30" value="<?php echo $database_password?>">
 				</td>
 			</tr>
 

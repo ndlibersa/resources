b8d4206439c1d8459d67377f9fe84f0ad65097a8
Douglas Hahn <dhahn@library.tamu.edu>
2012-09-24 12:37:34 -0500

    Change the install script to account for new tables

--- install/protected/install.sql
+++ install/protected/install.sql
@@ -291,6 +291,8 @@ CREATE TABLE  `_DATABASE_NAME_`.`Resource` (
   `bibSourceURL` VARCHAR( 2000 ) DEFAULT NULL ,
   `catalogingTypeID` int(11) DEFAULT NULL,
   `catalogingStatusID` int(11) DEFAULT NULL,
+  `coverageText` VARCHAR(1000) NULL DEFAULT NULL,
+  `resourceAltURL` VARCHAR(2000) NULL DEFAULT NULL,
   PRIMARY KEY  (`resourceID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
@@ -530,6 +532,39 @@ CREATE TABLE  `_DATABASE_NAME_`.`Workflow` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `_DATABASE_NAME_`.`GeneralSubject`;
+CREATE TABLE `_DATABASE_NAME_`.`GeneralSubject` (
+  `generalSubjectID` int(11) NOT NULL AUTO_INCREMENT,
+  `shortName` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`generalSubjectID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `_DATABASE_NAME_`.`DetailedSubject`;
+CREATE TABLE `_DATABASE_NAME_`.`DetailedSubject` (
+  `detailedSubjectID` int(11) NOT NULL AUTO_INCREMENT,
+  `shortName` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`detailedSubjectID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `_DATABASE_NAME_`.`GeneralDetailsubjectlink`;
+CREATE TABLE `_DATABASE_NAME_`.`GeneralDetailsubjectlink` (
+  `generalDetailSubjectLinkID` int(11) NOT NULL AUTO_INCREMENT,
+  `generalSubjectID` int(11) DEFAULT NULL,
+  `detailedSubjectID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`generalDetailSubjectLinkID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `_DATABASE_NAME_`.`ResourceSubject`;
+CREATE TABLE `_DATABASE_NAME_`.`ResourceSubject` (
+  `resourceSubjectID` int(11) NOT NULL AUTO_INCREMENT,
+  `resourceID` int(11) DEFAULT NULL,
+  `generalDetailSubjectLinkID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`resourceSubjectID`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
 
 ALTER TABLE `_DATABASE_NAME_`.`Alias` ADD INDEX `Index_resourceID`(`resourceID`),
  ADD INDEX `Index_aliasTypeID`(`aliasTypeID`),
@@ -593,8 +628,14 @@ ALTER TABLE `_DATABASE_NAME_`.`ResourceRelationship` ADD INDEX `Index_resourceID
  
 ALTER TABLE `_DATABASE_NAME_`.`Status` ADD INDEX `shortName` ( `shortName` );
 
- 
- 
+ALTER TABLE `_DATABASE_NAME_`.`GeneralSubject` ADD INDEX `generalSubjectID` ( `generalSubjectID` );
+
+ALTER TABLE `_DATABASE_NAME_`.`DetailedSubject` ADD INDEX `detailedSubjectID` ( `detailedSubjectID` );
+
+ALTER TABLE `_DATABASE_NAME_`.`GeneralDetailsubjectlink` ADD INDEX `generalDetailSubjectLinkID` ( `generalDetailSubjectLinkID` );
+
+ALTER TABLE `_DATABASE_NAME_`.`ResourceSubject` ADD INDEX `resourceSubjectID` ( `resourceSubjectID` ); 
+
  
 INSERT INTO `_DATABASE_NAME_`.`AccessMethod` (shortName) values ('Standalone CD');
 INSERT INTO `_DATABASE_NAME_`.`AccessMethod` (shortName) values ('External Host');

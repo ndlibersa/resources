0fc8478d540d191a669c14bf3135296b4f6e5fcc
Paul Hoffman <paul@flo.org>
2015-03-04 11:07:57 -0500

    Remove pointless _DATABASE_NAME_ prefixes

--- install/protected/update_1.2_cost_history.sql
+++ install/protected/update_1.2_cost_history.sql
@@ -1,8 +1,8 @@
-ALTER TABLE `_DATABASE_NAME_`.`Resource`
+ALTER TABLE `Resource`
   CHANGE COLUMN `subscriptionStartDate` `currentStartDate` date default NULL,
   CHANGE COLUMN `subscriptionEndDate` `currentEndDate` date default NULL;
 
-ALTER TABLE `_DATABASE_NAME_`.`ResourcePayment`
+ALTER TABLE `ResourcePayment`
   ADD COLUMN `year` varchar(20) default NULL,
   ADD COLUMN `subscriptionStartDate` date default NULL,
   ADD COLUMN `subscriptionEndDate` date default NULL,
@@ -10,21 +10,21 @@ ALTER TABLE `_DATABASE_NAME_`.`ResourcePayment`
   ADD COLUMN `costNote` text,
   ADD COLUMN `invoiceNum` varchar(20);
 
-DROP TABLE IF EXISTS `_DATABASE_NAME_`.`CostDetails`;
-CREATE TABLE `_DATABASE_NAME_`.`CostDetails` (
+DROP TABLE IF EXISTS `CostDetails`;
+CREATE TABLE `CostDetails` (
   `costDetailsID` int(11) NOT NULL AUTO_INCREMENT,
   `shortName` varchar(200) NOT NULL,
   PRIMARY KEY (`costDetailsID`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-ALTER TABLE `_DATABASE_NAME_`.`ResourcePayment`
+ALTER TABLE `ResourcePayment`
   DROP INDEX `Index_All`,
   ADD INDEX `Index_year`(`year`),
   ADD INDEX `Index_costDetailsID`(`costDetailsID`),
   ADD INDEX `Index_invoiceNum`(`invoiceNum`),
   ADD INDEX `Index_All`(`resourceID`, `fundName`, `year`, `costDetailsID`, `invoiceNum`);
 
-ALTER TABLE `_DATABASE_NAME_`.`CostDetails`
+ALTER TABLE `CostDetails`
   ADD INDEX `costDetailsID`(`costDetailsID`),
   ADD INDEX `Index_shortName`(`shortName`),
   ADD INDEX `Index_All`(`costDetailsID`, `shortName`);

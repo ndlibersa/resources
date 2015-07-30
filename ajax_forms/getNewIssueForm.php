1001da6c1c41d884ef049f041dd9bdefc5dccbb8
jeremythuff <jeremythuff@gmail.com>
2015-07-30 17:41:00 -0500

    Added options to the reminider interval selction

--- ajax_forms/getNewIssueForm.php
+++ ajax_forms/getNewIssueForm.php
@@ -97,8 +97,19 @@ if (!empty($organizationResourcesArray)) {
 		</tr>
 	</table>
 
-	<p> Send me a reminder in 
-		<select name="issue[reminderInterval]"></select> days 
+	<p> Send me a reminder every 
+		<select name="issue[reminderInterval]">
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
+			<option>6</option>
+			<option>7</option>
+			<option>8</option>
+			<option>9</option>
+			<option>10</option>
+		</select> day(s) 
 	</p>
 
 	<table class='noBorderTable' style='width:125px;'>

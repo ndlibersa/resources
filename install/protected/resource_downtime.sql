43defcbb5fd9fcea5ba73019c2be487cd7d241f0
jeremythuff <jeremythuff@gmail.com>
2015-08-10 15:53:58 -0500

    Added install script for downtime tables

---
+++ install/protected/resource_downtime.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE IF EXISTS `Downtime`;
+CREATE TABLE IF NOT EXISTS `Downtime` (
+`downtimeID` int(11) NOT NULL,
+  	`issueID` int(11) DEFAULT NULL,
+  	`downtimeStartdate` date NOT NULL,
+  	`downtimeEnddate` date DEFAULT NULL,
+  	`downtimeType` int(11) NOT NULL,
+  	PRIMARY KEY (`downtimeID`),
+  	KEY `IssueID` (`IssueID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS `DowntimeType`;
+CREATE TABLE IF NOT EXISTS `DowntimeType` (
+	`downtimeTypeID` int(11) NOT NULL,
+	`downtimeTypeName` varchar(80) NOT NULL,
+	PRIMARY KEY (`downtimeTypeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;

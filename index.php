4eaec02288a7b8bcb9c4514768a2564330bc7e07
Bill Dueber <bill@dueber.com>
2016-01-20 12:05:55 -0500

    can't use isset with a function result

--- index.php
+++ index.php
@@ -597,20 +597,20 @@ CoralSession::set('ref_script', $currentPage);
 <?php
   //used to default to previously selected values when back button is pressed
   //if the startWith is defined set it so that it will default to the first letter picked
-  if ((isset(CoralSession::get('res_startWith'))) && ($reset != 'Y')){
+  if ((CoralSession::get('res_startWith')) && ($reset != 'Y')){
 	  echo "startWith = '" . CoralSession::get('res_startWith') . "';";
 	  echo "$(\"#span_letter_" . CoralSession::get('res_startWith') . "\").removeClass('searchLetter').addClass('searchLetterSelected');";
   }
 
-  if ((isset(CoralSession::get('res_pageStart'))) && ($reset != 'Y')){
+  if ((CoralSession::get('res_pageStart')) && ($reset != 'Y')){
 	  echo "pageStart = '" . CoralSession::get('res_pageStart') . "';";
   }
 
-  if ((isset(CoralSession::get('res_recordsPerPage'))) && ($reset != 'Y')){
+  if ((CoralSession::get('res_recordsPerPage')) && ($reset != 'Y')){
 	  echo "recordsPerPage = '" . CoralSession::get('res_recordsPerPage') . "';";
   }
 
-  if ((isset(CoralSession::get('res_orderBy'))) && ($reset != 'Y')){
+  if ((CoralSession::get('res_orderBy')) && ($reset != 'Y')){
 	  echo "orderBy = \"" . CoralSession::get('res_orderBy') . "\";";
   }
 

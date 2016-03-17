74280733c78336800ea15b14baf6fbf76c671998
Bradley Droubay <brad.droubay@sirsidynix.com>
2016-03-17 10:24:21 -0600

    Import Changes -- Still in progress

--- ajax_forms/getAdminImportConfigUpdateForm.php
+++ ajax_forms/getAdminImportConfigUpdateForm.php
@@ -10,38 +10,104 @@
 		$orgMappingInstance = new OrgNameMapping();
 		$orgMappings=array();
 	}
+
+	//get all alias types for output in drop-down menu
+	$aliasTypeArray = array();
+	$aliasTypeObj = new AliasType();
+	$aliasTypeArray = $aliasTypeObj->allAsArray();
+
+	$configuration=json_decode($instance->configuration,true);
 ?>
-		<div id='div_updateForm'>
-		<?php print $orgMappingInstance->getOrgNameMappingByImportConfigID($updateID)[0]->orgNameImported;
-		      print $orgMappingInstance->getOrgNameMappingByImportConfigID($updateID)[1]->orgNameImported;
-		?>
-
+	<div id='div_updateForm'>
 		<input type='hidden' id='imortConfigID' value='<?php echo $updateID; ?>'>
-
-		<div class='formTitle' style='min-width:485px;'><span class='headerText' style='margin-left:7px;'><?php if ($updateID){ echo "Edit Import Configuration"; } else { echo "Add Import Configuration"; } ?></span></div>
-
-		<span class='smallDarkRedText' id='span_errors'></span>
-		<div id='importConfigColumns'>
-			<div style="float:left; width:50%">
-				Left Content
+		<div class='formTitle' style='min-width:1000px;'><span class='headerText' style='margin-left:7px;'><?php if ($updateID){ echo "Edit Import Configuration"; } else { echo "Add Import Configuration"; } ?></span></div>
+			<span class='smallDarkRedText' id='span_errors'></span>
+			<div id='importConfigColumns'>
+				<div id='importConfigColumnsLeft'>
+					<p><span>Resource Title</span><span class='import-text-left'><input value="<?php echo $configuration["title"]?>" /></span></p>
+					<div id='resource_alias'>
+						<?php
+							if(count($configuration["alias"]) > 0) {
+								foreach($configuration["alias"] as $alias) {
+									echo "<p><span>Alias</span><span><input value='".$alias."' /></span><span><select>";
+									foreach($aliasTypeArray as $aliasType) {
+										echo "<option value='" . $aliasType['aliasTypeID'] . "'>" . $aliasType['shortName'] . "</option>";
+									}
+									echo "</select></span></p>";
+								}
+							}
+							else {
+								echo "<p><span>Alias</span><span><input value='' /></span><span><select>";
+								foreach($aliasTypeArray as $aliasType) {
+									echo "<option value='" . $aliasType['aliasTypeID'] . "'>" . $aliasType['shortName'] . "</option>";
+								}
+								echo "</select></span></p>";
+							}
+						?>
+						<p><a href='#'>+ Add another alias</a></p>
+					</div>
+					<p><span>Resource URL</span><span><input value="<?php echo $configuration["url"]?>" /></span></p>
+					<p><span>Alternate URL</span><span><input value="<?php echo $configuration["altUrl"]?>" /></span></p>
+					<div id='resource_parent'>
+						<?php
+							if(count($configuration["parent"]) > 0) {
+								foreach($configuration["parent"] as $parent) {
+									echo "<p><span>Parent Resource</span><span><input value='" . $parent . "' /></span></p>";
+								}
+							}
+							else {
+								echo "<p><span>Parent Resource</span><span><input value='' /></span></p>";
+							}
+						?>
+						<p><a href='#'>+ Add another parent resource</a></p>
+					</div>
+					<div id='resource_isbnOrIssn'>
+						<?php
+							if(count($configuration["isbnOrIssn"]) > 0) {
+								foreach($configuration["isbnOrIssn"] as $isbnOrIssn) {
+									echo "<p><span>ISBN or ISSN</span><span><input value='" . $isbnOrIssn . "' /></span></p>";
+								}
+							}
+							else {
+								echo "<p><span>ISBN OR ISSN</span><span><input value='' /></span></p>";
+							}
+						?>
+						<p><a href='#'>+ Add another ISBN or ISSN</a></p>
+					</div>
+					<p><span>Resource Format</span><span><input value="<?php echo $configuration["resourceFormat"]?>" /></span></p>
+					<p><span>Resource Type</span><span><input value="<?php echo $configuration["resourceType"]?>" /></span></p>
+					<div id='resource_subject'>
+						<?php
+							if(count($configuration["subject"]) > 0) {
+								foreach($configuration["subject"] as $subject) {
+									echo "<p><span>Subject</span><span><input value='" . $subject . "' /></span></p>";
+								}
+							}
+							else {
+								echo "<p><span>Subject</span><span><input value='' /></span></p>";
+							}
+						?>
+						<p><a href='#'>+ Add another subject</a></p>
+					</div>
+				</div>
+				<div id='importConfigColumnsRight'>
+					Right Content
+				</div>
+				<div style='clear: both;''></div>
 			</div>
-			<div style="float:right; width:50%">
-				Right Content
-			</div>
-		</div>
-		<div id='importConfigOrgMapping'>
-			<table>
-				<tr>
-					<th>Text in CSV File</th>
-					<th>Will Change To</th>
-				</tr>
+			<div id='importConfigOrgMapping'>
+				<table>
+					<tr>
+						<th>Text in CSV File</th>
+						<th>Will Change To</th>
+					</tr>
 <?php
 	foreach($orgMappings as $orgMapping) {
-		echo "<tr><td>".$orgMapping->orgNameImported."</td><td>".$orgMapping->orgNameMapped."</td></tr>";
+		echo "<tr><td><input value='".htmlspecialchars($orgMapping->orgNameImported)."' /></td><td><input value='".htmlspecialchars($orgMapping->orgNameMapped)."' /></td></tr>";
 	}
 ?>
-			</table>
-		</div>
+				</table>
+			</div>
 
 		<!--
 		<table class="surroundBox" style="width:490px;">
--- ajax_processing/loadImportConfiguration.php
+++
@@ -1,9 +0,0 @@
-<?php
-$configName = $_POST["configName"];
-
-//$infile = fopen("import_configs/" . $configName, "rb");
-$infile = fopen("import_configs/" . $configName, "rb");
-$configData = fgets($infile,1000);
-fclose($infile);
-echo $configData;
-?>

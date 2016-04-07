6a69a7f6a93753fb597270d680b7068a6778b999
Bradley Droubay <brad.droubay@sirsidynix.com>
2016-04-07 13:41:05 -0600

    Fixed bindtextdomain path

--- directory.php
+++ directory.php
@@ -194,7 +194,7 @@ if(isset($_COOKIE["lang"])){
 }
 putenv("LC_ALL=$http_lang");
 setlocale(LC_ALL, $http_lang.".utf8");
-bindtextdomain("messages", "./locale");
+bindtextdomain("messages", dirname(__FILE__) . "/locale");
 textdomain("messages");
 
 ?>

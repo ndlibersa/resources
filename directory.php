d2977f4427020a9dd2be65ee5771af33c45af920
Bradley Droubay <brad.droubay@sirsidynix.com>
2016-04-11 12:36:56 -0600

    fixed changed default locale to get 5 characters instead of 2

--- directory.php
+++ directory.php
@@ -189,8 +189,7 @@ global $http_lang;
 if(isset($_COOKIE["lang"])){
     $http_lang = $_COOKIE["lang"];
 }else{        
-    $codeL = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5);
-    echo "<b>" . $codeL . "</b>";
+    $codeL = str_replace("-","_",substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5));
     $http_lang = $lang_name->getLanguage($codeL);
 }
 putenv("LC_ALL=$http_lang");

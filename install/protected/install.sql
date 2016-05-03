5f7b00da76e9858ddb9dcb8738e0c795fff5668d
Remington Steed <rjs7@calvin.edu>
2016-05-03 09:59:01 -0400

    Prepare install script for Resources 1.4.0
    This commit prepares the update_1.4.sql file and updates the base
    install.sql script.
    
    Signed-off-by: Remington Steed <rjs7@calvin.edu>

--- install/protected/install.sql
+++ install/protected/install.sql
@@ -591,6 +591,83 @@ CREATE TABLE `CostDetails` (
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `Issue`;
+CREATE TABLE `Issue` (
+  `issueID` int(11) NOT NULL AUTO_INCREMENT,
+  `creatorID` varchar(20) NOT NULL,
+  `subjectText` varchar(80) NOT NULL,
+  `bodyText` text NOT NULL,
+  `reminderInterval` int(11) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  `dateClosed` datetime DEFAULT NULL,
+  `resolutionText` text,
+  PRIMARY KEY (`issueID`),
+  KEY `creatorID` (`creatorID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS `IssueRelationship`;
+CREATE TABLE `IssueRelationship` (
+  `issueRelationshipID` int(11) NOT NULL AUTO_INCREMENT,
+  `issueID` int(11) NOT NULL,
+  `entityID` int(11) NOT NULL,
+  `entityTypeID` int(11) NOT NULL,
+  PRIMARY KEY (`issueRelationshipID`),
+  KEY `issueID` (`issueID`,`entityID`,`entityTypeID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+
+DROP TABLE IF EXISTS `IssueEntityType`;
+CREATE TABLE `IssueEntityType` (
+  `entityTypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `entityName` varchar(80) NOT NULL,
+  PRIMARY KEY (`entityTypeID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+
+DROP TABLE IF EXISTS `IssueContact`;
+CREATE TABLE `IssueContact` (
+  `issueContactID` int(11) NOT NULL AUTO_INCREMENT,
+  `issueID` int(11) NOT NULL,
+  `contactID` int(11) NOT NULL,
+  PRIMARY KEY (`issueContactID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+
+DROP TABLE IF EXISTS `IssueEmail`;
+CREATE TABLE `IssueEmail` (
+  `issueEmailID` int(11) NOT NULL AUTO_INCREMENT,
+  `issueID` int(11) NOT NULL,
+  `email` varchar(120) NOT NULL,
+  PRIMARY KEY (`IssueEmailID`),
+  KEY `IssueID` (`IssueID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `Downtime`;
+CREATE TABLE IF NOT EXISTS `Downtime` (
+  `downtimeID` int(11) NOT NULL AUTO_INCREMENT,
+  `issueID` int(11) DEFAULT NULL,
+  `entityID` int(11) NOT NULL,
+  `entityTypeID` int(11) NOT NULL DEFAULT '2',
+  `creatorID` varchar(80) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime NOT NULL,
+  `downtimeTypeID` int(11) NOT NULL,
+  `note` TEXT DEFAULT NULL,
+   PRIMARY KEY (`downtimeID`),
+   KEY `IssueID` (`IssueID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS `DowntimeType`;
+CREATE TABLE IF NOT EXISTS `DowntimeType` (
+  `downtimeTypeID` int(11) NOT NULL AUTO_INCREMENT,
+  `shortName` varchar(80) NOT NULL,
+  PRIMARY KEY (`downtimeTypeID`)
+) ENGINE=MyISAM DEFAULT CHARACTER SET = utf8 COLLATE = utf8_general_ci AUTO_INCREMENT=1;
+
+
+
+
 ALTER TABLE `Alias` ADD INDEX `Index_resourceID`(`resourceID`),
  ADD INDEX `Index_aliasTypeID`(`aliasTypeID`),
  ADD INDEX `shortName` ( `shortName` ),
